8b9bdee580f6af3462c60a38a12f1d23461c9870
tensho1026 <tensho1026@icloud.com>
2025-03-06 22:47:34 +0900

    add/folder

---
+++ src/app/(dashboard)/parent/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ParentPage = () => {
+  return <div>page</div>;
+};
+
+export default ParentPage;
---
+++ src/app/(dashboard)/teacher/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeacherPage = () => {
+  return <div>page</div>;
+};
+
+export default TeacherPage;
