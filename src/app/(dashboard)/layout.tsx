17722eaf8ad954db7b06ecbcd7733cd3cb385201
tensho1026 <tensho1026@icloud.com>
2025-03-08 21:48:44 +0900

    feature/navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -10,7 +11,7 @@ export default function DashboardLayout({
   return (
     <div className='h-screen flex'>
       {/* LEFT */}
-      <div className='w-[14%] md:w-[8%] lg:w-[14%] xl:w-[14%] bg-red-200 p-4'>
+      <div className='w-[14%] md:w-[8%] lg:w-[14%] xl:w-[14%]  p-4'>
         <Link href='/' className="flex items-center justify-center lg:justify-start gap-2">
           <Image src='/logo.png' alt='logo' width={32} height={32} />
           <span className="hidden lg:block">SchoolLama</span>
@@ -18,8 +19,9 @@ export default function DashboardLayout({
         <Menu/>
       </div>
       {/* RIGHT */}
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200'>
-        r
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]  bg-[#F7F8FA] overflow-scroll'>
+        <Navbar/>
+        {children}
       </div>
     </div>
   );
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -116,20 +116,21 @@ function Menu() {
       ],
     },
   ];
-  return <div>
-    {menuItems.map(i=>(
-      <div className="" key={i.title}>
-        <span>{i.title}</span>
-        {i.items.map(item=>(
-          <Link href={item.href} key={item.label}>
-          <Image src={item.icon} alt="" width={20} height={20}/>
-          <span>{item.label}</span>
-          <p>kjfojofopjfo</p>
-          </Link>
-        ))}
-      </div>
-    ))}
-  </div>;
+  return (
+    <div>
+      {menuItems.map((i) => (
+        <div className='' key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map((item) => (
+            <Link href={item.href} key={item.label} className="flex  items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={item.icon} alt='' width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Menu;
