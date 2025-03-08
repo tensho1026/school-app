fda4300988833612ea9bb8f5e48e79ff9429fa45
tensho1026 <tensho1026@icloud.com>
2025-03-08 22:25:30 +0900

    fix/code

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,15 +12,18 @@ export default function DashboardLayout({
     <div className='h-screen flex'>
       {/* LEFT */}
       <div className='w-[14%] md:w-[8%] lg:w-[14%] xl:w-[14%]  p-4'>
-        <Link href='/' className="flex items-center justify-center lg:justify-start gap-2">
+        <Link
+          href='/'
+          className='flex items-center justify-center lg:justify-start gap-2'
+        >
           <Image src='/logo.png' alt='logo' width={32} height={32} />
-          <span className="hidden lg:block">SchoolLama</span>
+          <span className='hidden lg:block'>SchoolLama</span>
         </Link>
-        <Menu/>
+        <Menu />
       </div>
       {/* RIGHT */}
       <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]  bg-[#F7F8FA] overflow-scroll'>
-        <Navbar/>
+        <Navbar />
         {children}
       </div>
     </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -120,11 +120,17 @@ function Menu() {
     <div>
       {menuItems.map((i) => (
         <div className='' key={i.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          <span className='hidden lg:block text-gray-400 font-light my-4'>
+            {i.title}
+          </span>
           {i.items.map((item) => (
-            <Link href={item.href} key={item.label} className="flex  items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+            <Link
+              href={item.href}
+              key={item.label}
+              className='flex  items-center justify-center lg:justify-start gap-4 text-gray-500 py-2'
+            >
               <Image src={item.icon} alt='' width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
+              <span className='hidden lg:block'>{item.label}</span>
             </Link>
           ))}
         </div>
