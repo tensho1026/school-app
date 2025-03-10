5ebc752e822183b9a20b3fd17fc2dc93a4139b83
tensho1026 <tensho1026@icloud.com>
2025-03-10 23:29:20 +0900

    feature/AnimatedLineChart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
 
@@ -27,7 +28,9 @@ const AdminPage = () => {
           </div>
         </div>
         {/* {BOTTOM CHARTS0} */}
-        <div className=''></div>
+        <div className='w-full h-[500px]'>
+          <FinanceChart/>
+        </div>
       </div>
       {/* {RIGHT} */}
       <div className='w-full lg:w-1/3'>r</div>
--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -53,13 +53,16 @@ function CountChart() {
             barSize={32}
             data={data}
           >
-            <RadialBar
-              background
-              dataKey='count'
-            />
+            <RadialBar background dataKey='count' />
           </RadialBarChart>
         </ResponsiveContainer>
-        <Image src="/maleFemale.png" alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
+        <Image
+          src='/maleFemale.png'
+          alt=''
+          width={50}
+          height={50}
+          className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+        />
       </div>
       {/* {BOTTOM} */}
       <div className='flex justify-center gap-16'>
