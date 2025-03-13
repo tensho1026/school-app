7f4a4f9f661e3215c17020847b5149d2db0f864a
tensho1026 <tensho1026@icloud.com>
2025-03-13 21:36:00 +0900

    create/listTableFile

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TeacherList() {
+  return (
+    <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+      {/* {TOP} */}
+     <div className=""></div>
+    </div>
+  )
+}
+
+export default TeacherList
