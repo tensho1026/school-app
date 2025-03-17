1fba9718ad980a066faa69f4ea469fd796a3c4fb
tensho1026 <tensho1026@icloud.com>
2025-03-17 23:55:33 +0900

    feature/React-Hook-Form

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,15 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import Teacherform from "./forms/Teacherform";
+import Studentform from "./forms/StudentForm";
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <Teacherform type={type} data={data} />,
+  student: (type, data) => <Studentform type={type} data={data} />,
+};
 
 function FormModal({
   table,
@@ -37,14 +46,16 @@ function FormModal({
 
   const Form = () => {
     return type === "delete" && id ? (
-      <form action='' className="p-4 flex flex-col gap-4">
-        <span className="text-center font-medium">
+      <form action='' className='p-4 flex flex-col gap-4'>
+        <span className='text-center font-medium'>
           All data will be lost. Are you sure you want to delete this {table}?
         </span>
-        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
+        <button className='bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center'>
+          Delete
+        </button>
       </form>
     ) : (
-      "create or update form"
+      <Studentform type={"create"} />
     );
   };
 
@@ -59,7 +70,7 @@ function FormModal({
       {open && (
         <div className='w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center'>
           <div className='bg-white p-4 rounded-md relative  w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%]  2xl:w-[40%]'>
-            <Form/>
+            <Form />
             <div
               className='absolute top-4 right-4 cursor-pointer'
               onClick={() => setOpen(false)}
